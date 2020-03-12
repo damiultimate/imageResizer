38c5c6317a04f2786b12750300297f9606e10e97
Adeoti Damilola <57512119+damiultimate@users.noreply.github.com>
2020-03-11 17:12:52 -0700

    Update makenew.py

--- makenew.py
+++ makenew.py
@@ -1,4 +1,4 @@
-#!C:\Users\ORE\AppData\Local\Programs\Python\Python38-32\python.exe
+#!C:\Users\User\AppData\Local\Programs\Python\Python38-32\python.exe
 import os, cgi, cgitb; 
 from PIL import Image
 from resizeimage import resizeimage
